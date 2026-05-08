4d9ee2fd253ba5087710f0b40b40c02f4f0b2824
NutellaNaf <67666242+NutellaNaf@users.noreply.github.com>
2026-05-08 15:07:24 -0500

    Editing app.py (flask) paths to use SQL api

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def top_n_columns(column_of_interest, n):
         return_string = """"""
         # results = top_n_column_values(column_of_interest, n)
         results = database.get_top_n_column_values_SQL(conn, column_of_interest, n)
-        return_string += f"<h1> Top 5 Models by {column_of_interest} </h1>"
+        return_string += f"<h1> Top {n} Models by {column_of_interest} </h1>"
         for result in results:
             return_string += "<p> Model:" + str(result[0]) + f" | {result[1]} </p>"
         return return_string
@@ -44,9 +44,29 @@ def top_n_columns(column_of_interest, n):
 def fetch_column(column_of_interest):
     try:
         return_string = """"""
-        results = fetch_column_data(column_of_interest)
+        # results = fetch_column_data(column_of_interest)
+        results = database.get_whole_column_SQL(conn, column_of_interest)
+        return_string += f"<h1> All values in {column_of_interest} </h1>"
+        return_string += "<ol>"
         for result in results:
-            return_string += "<p> " + str(result[0]) + f" in row {result[1]} </p>"
+            return_string += f"<li> {result} </li>"
+        return_string += "</ol>"
+        return return_string
+
+    except ValueError:
+        return abort(404)
+
+@app.route('/model/<string:model_name>')
+def row_by_model(model_name):
+    try:
+        return_string = """"""
+        # results = fetch_column_data(column_of_interest)
+        results = database.get_row_by_model_name_SQL(conn, model_name)
+        return_string += f"<h1> Info for {model_name} </h1>"
+        # return_string += "<ol>"
+        for result in results:
+            return_string += f"<p> {result} </p>"
+        # return_string += "</ol>"
         return return_string
 
     except ValueError:
